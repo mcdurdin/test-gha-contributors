dc481c83183553bcbeb67327b24b0117c6c042ce
Marc Durdin <marc@durdin.net>
2026-01-19 08:40:03 +1100

    msg

--- _scripts/contributors/run.sh
+++ _scripts/contributors/run.sh
@@ -5,6 +5,8 @@
 npm ci
 node . -g "${GITHUB_TOKEN}" -c "${CROWDIN_TOKEN}" -o "../../about/contributors.md"
 if ! git diff --exit-code -- ../../about/contributors.md; then
+  git config user.email server@keyman.com
+  git config user.name "Keyman Server"
   git switch -c auto/contributors
   git add ../../about/contributors.md
   git commit -m "auto: refresh contributors.md
