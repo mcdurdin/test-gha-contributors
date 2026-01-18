2ea17631600a47d8fb6badba5feadeef0e71d7a3
Marc Durdin <marc@durdin.net>
2026-01-19 08:32:34 +1100

    auto

---
+++ _scripts/contributors/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# assumes run from current folder
+
+npm ci
+node . -g "${GITHUB_TOKEN}" -c "${CROWDIN_TOKEN}" -o "../../about/contributors.md"
+if gif diff --exit-code ../../about/contributors.md; then
+  git switch -c auto/contributors
+  git add ../../about/contributors.md
+  git commit -m "auto: refresh contributors.md
+
+Test-bot: skip"
+  git push -u origin auto/contributors
+  gh pr create --fill
+fi
